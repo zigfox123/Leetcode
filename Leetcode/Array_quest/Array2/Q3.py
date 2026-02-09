9258fc6b17b059d790e9168753371ad53f31d7b9
zigfox <cork.darragh@gmail.com>
2026-02-09 14:47:47 +0000

    Completed the Leetcode quest for data structures and algorithms Array II Q3 in python

---
+++ Leetcode/Array_quest/Array2/Q3.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        return [i for i in range(1, len(nums) + 1) if i not in s]
+
+#Accepted answer
