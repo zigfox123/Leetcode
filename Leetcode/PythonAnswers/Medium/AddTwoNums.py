9b051f0100fd467f17681fab5c6af175797c9a22
zigfox <cork.darragh@gmail.com>
2026-02-10 17:51:57 +0000

    Fixing code

--- Leetcode/PythonAnswers/Medium/AddTwoNums.py
+++ Leetcode/PythonAnswers/Medium/AddTwoNums.py
@@ -1,5 +1,10 @@
 from typing import Optional
 
+# Definition for singly-linked list.
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         output = ListNode(0)
