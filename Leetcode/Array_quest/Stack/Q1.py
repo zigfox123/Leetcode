c06bfd91af19dfca4b40932ccf6045989c17f6c6
zigfox <cork.darragh@gmail.com>
2026-02-18 17:05:45 +0000

    Completed leetcode Arrayquest Stack Q1 in python

--- Leetcode/Array_quest/Stack/Q1.py
+++ Leetcode/Array_quest/Stack/Q1.py
@@ -2,4 +2,14 @@ from typing import List
 
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
-        return
+        output = []
+        target_set = set(target)
+        
+        for i in range(1, target[-1] + 1):
+            output.append("Push")
+            if i not in target_set:
+                output.append("Pop")
+        
+        return output
+    
+#Accepted answer
