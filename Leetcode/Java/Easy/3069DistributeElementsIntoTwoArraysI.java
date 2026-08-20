c2d3c06078cdd5be0453e4d432e76e48d3936d2e
zigfox <cork.darragh@gmail.com>
2026-08-20 11:52:51 +0100

    daily leetcode question in java

---
+++ Leetcode/Java/Easy/3069DistributeElementsIntoTwoArraysI.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution{
+    public int[] resultArray(int[] nums){
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        int result[] = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            if(i == 0){
+                arr1.add(nums[i]);
+            }else if(i == 1){
+                arr2.add(nums[i]);
+            }
+            else if(arr1.get(arr1.size() - 1) > arr2.get(arr2.size() -1)){
+                arr1.add(nums[i]);
+            }else{
+                arr2.add(nums[i]);
+            }
+        
+        for(int j = 0; j < arr1.size(); j++){
+            result[j] = arr1.get(j);
+        }
+        for(int k = 0; k < arr2.size(); k++){
+            result[arr1.size() + k] = arr2.get(k);
+        }
+        }
+    return result;
+        
+    }
+}
+//Accepted solution
